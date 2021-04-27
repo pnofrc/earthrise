40c6cc6b661b4f93788005b24ff1942bd78270ed
pnofrc <pnofrc@pnofrclocal.local>
2021-04-27 13:28:48 +0200

    ready to test

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
 // DICTIONARIES
 var coord = {
-    "x1": [51.914444, 4.470009], //DW
-    "x2": [51.906289, 4.442124], //DW
-    "x3": [51.906333, 4.492452], //DF
-    "x4": [51.928074, 4.476836], //DF
+    "x1": [51.914444, 4.470009], //DF
+    "x2": [51.906289, 4.442124], //DF
+    "x3": [51.906333, 4.492452], //DW
+    "x4": [51.928074, 4.476836], //DW
     "x5": [51.912516, 4.501322], //PG
     "x6": [51.928861, 4.480778], //PG
     "x7": [51.916006, 4.476677] //END
 }
 
 var audioWhile = {
-  "LatLng(51.914444, 4.470009)":"audio/audioWhile/dolphinwaves.mp3", //DW
-  "LatLng(51.906289, 4.442124)":"audio/audioWhile/dolphinwaves.mp3", //DW
-  "LatLng(51.906333, 4.492452)":"audio/audioWhile/dragonfly.mp3", //DF
-  "LatLng(51.928074, 4.476836)":"audio/audioWhile/dragonfly.mp3", //DF
+  "LatLng(51.914444, 4.470009)":"audio/audioWhile/dragonfly.mp3", //DF
+  "LatLng(51.906289, 4.442124)":"audio/audioWhile/dragonfly.mp3", //DF
+  "LatLng(51.906333, 4.492452)":"audio/audioWhile/dolphinwaves.mp3", //DW
+  "LatLng(51.928074, 4.476836)":"audio/audioWhile/dolphinwaves.mp3", //DW
   "LatLng(51.912516, 4.501322)":"audio/audioWhile/progectgecko.mp3", //PG
   "LatLng(51.928861, 4.480778)":"audio/audioWhile/progectgecko.mp3", //PG
   "LatLng(51.916006, 4.476677)":"audio/outro.mp3"
 }
 
 var audioWhileX = {
-  "x1":"audio/audioWhile/dolphinwaves.mp3", //DW
-  "x2":"audio/audioWhile/dolphinwaves.mp3", //DW
-  "x3":"audio/audioWhile/dragonfly.mp3", //DF
-  "x4":"audio/audioWhile/dragonfly.mp3", //DF
+  "x1":"audio/audioWhile/dragonfly.mp3", //DF
+  "x2":"audio/audioWhile/dragonfly.mp3", //DF
+  "x3":"audio/audioWhile/dolphinwaves.mp3", //DW
+  "x4":"audio/audioWhile/dolphinwaves.mp3", //DW
   "x5":"audio/audioWhile/progectgecko.mp3", //PG
   "x6":"audio/audioWhile/progectgecko.mp3", //PG
   "x7":"audio/outro.mp3"
@@ -40,43 +40,54 @@ var getSymb = {
 }
 
 var codeword = {
-  "x1": 'TEST', //DW
-  "x2": 'TEST', //DW
-  "x3": 'TEST', //DF
-  "x4": 'TEST', //DF
-  "x5": 'TEST', //PG
-  "x6": 'TEST', //PG
-  "x7": 'TEST' //END
+  "x1": 'SAFETY', //DF
+  "x2": 'DESIGN', //DF
+  "x3": 'FLOTATION', //DW
+  "x4": 'AUTOMATION', //DW
+  "x5": 'EMBODIED', //PG
+  "x6": 'VISIBLE', //PG
+  "x7": 'COEXIST' //END
 }
 
 var title = {
-    "x1":"Dragonfly // Het Nieuwe Instituut", //DW
-    "x2":"Dragonfly // Dakpark", //DW
-    "x3":"Dolphin Waves // Floating Pavilion", //DF
-    "x4":"Dolphin Waves // Benthemplein", //DF
+    "x1":"Dragonfly // Het Nieuwe Instituut", //DF
+    "x2":"Dragonfly // Dakpark", //DF
+    "x3":"Dolphin Waves // Floating Pavilion", //DW
+    "x4":"Dolphin Waves // Benthemplein", //DW
     "x5":"Project Gecko // Hef Park", //PG
     "x6":"Project Gecko // Almondestraat", //PG
     "x7":"The Roof" //END
 }
 
+var textQuest = {
+    "x1":"When the world suffers a collective trauma, and every step on every street reminds you of the harm and pain, what do you do? Do we depend on individuals and our local communities to slowly heal and grow again, or might we depend on artificial influences to stimulate our wellbeing? Should we put hurting memories on scaffolding, building over them to try to erase the pain, or do we leave stretch marks of change?  ", //DF
+    "x2":"Just like the bees and the flowers, we’ve helped humans connect with each other beyond the flowerbeds they would have otherwise stayed in. Letting flowers root beyond the concrete boxes designed for them, finding ways for global ecosystems and collectives to harmonize. Would the bee be a welcome presence in your flowerbed? Or might we have rooted enough?", //DF
+    "x3":"I am trying to read what you are thinking right now. I am watching you from inside the pavilion. I wonder, what would your game world look like? Would you feel this same sensation of excitement of discovering yourself everyday anew? Would you, like I did, move beyond ideas in which only the physical is real and immerse yourself in infinite worlds? Would you allow artificial influences to help you shape realities?", //DW
+    "x4":"Would you agree that everything you can think of is a product of an entire world of influences surrounding you? If so, is your imagination really yours to keep? Might it feel extremely liberating, to leave behind all worries about labour for profit for income to survive, in exchange for a limitless access to your thinking? Or are your thoughts really the sole things that truly are pricelessly yours?", //DW
+    "x5":"Was the chicken first or the egg? ", //PG
+    "x6":"Would you rather fight a horse sized duck or 100 duck sized horses?", //PG
+    "x7":""
+}
+
 var audioQuest = {
-    "x1":"audio/audioQuest/intro.wav", //DW
-    "x2":"audio/audioQuest/intro.wav", //DW
-    "x3":"audio/audioQuest/intro.wav", //DF
-    "x4":"audio/audioQuest/intro.wav", //DF
-    "x5":"audio/audioQuest/intro.wav", //PG
-    "x6":"audio/audioQuest/intro.wav", //PG
-    "x7":"audio/outro.mp3"
+  "x1":"audio/audioQuest/DF_hetnieuwe.mp3", //DW
+  "x2":"audio/audioQuest/DF_dakpark.mp3", //DW
+  "x3":"audio/audioQuest/intro.wav", //DF
+  "x4":"audio/audioQuest/intro.wav", //DF
+  "x5":"audio/audioQuest/intro.wav", //PG
+  "x6":"audio/audioQuest/intro.wav", //PG
+  "x7":"audio/outro.mp3"
 }
 
 var button = {
-    "x1":["1","2"], //DW
-    "x2":["1","2"], //DW
-    "x3":["1","2"], //DF
-    "x4":["1","2"], //DF
-    "x5":["1","2"], //PG
-    "x6":["1","2"], //PG
-    "x7":["1","2"]
+    "x1":["Build Over Them","Leave Stretch Marks"], //DW
+    "x2":["Welcome the Bee","We Have Rooted Enough"], //DW
+    "x3":["Immerse Yourself in Infinite Worlds","Stay Grounded Inside Physical World"],//DF,
+    "x4":["Give Limitless Access in Exchange for Freedom of Labour","Work for Your Money in Order to Keep Ownership of Your Thoughts"], //DF
+    "x5":["Chicken","Egg"], //PG
+    "x6":["100 Horses","1 Horse-Sized Ducks"], //PG
+    "x7":""
+
 }
 
 // BLACK POINT
@@ -110,7 +121,7 @@ var customIcon= {
   "p4":['worlds/Dragon_Fly/2-01.svg','worlds/Dragon_Fly/2-02.svg','worlds/Dragon_Fly/DragonFly.svg'],
   "p5":["worlds/Project_Gecko/1-01.svg","worlds/Project_Gecko/1-02.svg","worlds/Project_Gecko/ProjectGecko.svg"],
   "p6":["worlds/Project_Gecko/2-01.svg","worlds/Project_Gecko/2-02.svg","worlds/Project_Gecko/ProjectGecko.svg"],
-  
+  "p7":""
 }
 
 var customIconBack={
@@ -119,7 +130,8 @@ var customIconBack={
   "x3": "worlds/bck/DF.png",
   "x4": "worlds/bck/DF.png",
   "x5": "worlds/bck/PG.png",
-  "x6": "worlds/bck/PG.png"
+  "x6": "worlds/bck/PG.png",
+  "x7": ""
 }
 
 var worldIcon= {
@@ -132,14 +144,14 @@ var worldIcon= {
   "x7": ""
 }
 
-
+let array = ["x1","x2","x3","x4","x5","x6"];
 
 // SHUFFLE
 function shuffleArray(arr) {
     arr.sort(() => Math.random() - 0.5);
   }
 
-let arr = ["x1","x2","x3","x4","x5","x6"];
+shuffleArray(array)
 
 
 // COOKIES
@@ -161,10 +173,10 @@ if  (Object.entries(co).length === 0){
 
 function checkCookies(){
   if (Object.entries(co).length === 0){
-    // let shuffleArr = shuffleArray(arr)
-    arr.push('x7')
+     //var shuffleArr = shuffleArray(arr)
+    array.push('x7')
     for (c=0; c<7; c++){
-      let r = arr[c].toString() 
+      let r = array[c] 
       Cookies.set(r,r,  { expires: 60 })}
   }
 }
@@ -221,6 +233,7 @@ function showCustomGraph(){
 
 // SET CURRENT STATE IN HTML
 $('#quest').attr('src', audioQuest[current]);
+$('#textquest').append(textQuest[current])
 $("#title").append(title[current]);
 
 $("#firstChoice").append(button[current][0]);
